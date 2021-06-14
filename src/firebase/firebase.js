411350fbf049055c23a08d77d312facebff5091d
Abhay Vincent <abhaykvincent@gmail.com>
2021-06-14 04:03:56 -0400

    Firebase Setup
    Fixing Google SignIn

---
+++ src/firebase/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+const firebaseConfig = {
+    apiKey: "AIzaSyAzIshqL_NcjPfwo51uH074EbK9iqhB2NI",
+    authDomain: "dalhousie1881.firebaseapp.com",
+    projectId: "dalhousie1881",
+    storageBucket: "dalhousie1881.appspot.com",
+    messagingSenderId: "689858751941",
+    appId: "1:689858751941:web:96617639489e6d0f02afac",
+    measurementId: "G-Q1TT77B48W"
+  };
+  export const Firebase = firebase.initializeApp(firebaseConfig)
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
+import { Firebase } from '../../firebase/firebase'
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase";
 import './Login.scss'
-
+var provider = new Firebase.auth.GoogleAuthProvider();
 function Login() {
     return (
         <div className="login">
             <div className="login__google-apple-android">
-                    <div className="login__google">Google</div>
-                    <div className="login__apple">Apple</div>
+                    <div className="login__google" onClick={()=>{
+                        const auth = getAuth();
+                        signInWithPopup(auth, provider)
+                        .then((result) => {
+                          // This gives you a Google Access Token. You can use it to access the Google API.
+                          const credential = GoogleAuthProvider.credentialFromResult(result);
+                          const token = credential.accessToken;
+                          // The signed-in user info.
+                          const user = result.user;
+                          // ...
+                        }).catch((error) => {
+                          // Handle Errors here.
+                          const errorCode = error.code;
+                          const errorMessage = error.message;
+                          // The email of the user's account used.
+                          const email = error.email;
+                          // The AuthCredential type that was used.
+                          const credential = GoogleAuthProvider.credentialFromError(error);
+                          // ...
+                        });
+                      
+                    }}>Google</div>
+                    <div className="login__applenpm i firebase
+                    ">Apple</div>
             </div>
             <div className="login__email-password"></div>
         </div>
